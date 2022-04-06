9eeb9571b60ee2a98932f9450d228948378ddf09
oliviacorio <oliviacorio11@gmail.com>
2022-04-06 19:41:59 +0000

    proyecto terminado JS

--- src/app.js
+++ src/app.js
@@ -4,8 +4,30 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+// Definir las variables
+
+let who = ["El perro", "Mi abuela", "Mi profesor", "Mi motosierra"];
+let action = [" destruyo", " incinero", " pulverizo", " rompio"];
+let what = [" mi tarea", " mi ropa", " mi carrito", " mi casa"];
+let when = [
+  " el año pasado",
+  " ayer",
+  " hoy en la noche",
+  " durante la cuarentena"
+];
+
+function selectRandom(arr) {
+  let random = ~~(Math.random() * arr.length);
+  return arr[random];
+}
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let result = `${selectRandom(who)} ${selectRandom(action)}${selectRandom(
+    what
+  )}${selectRandom(when)}`;
+
+  let element = document.getElementById("excuse");
+  element.innerHTML = result;
 };
